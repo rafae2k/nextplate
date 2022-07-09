d3eea273aa0942d9770be7516553eeae68f5d8c0
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-09 13:05:44 -0300

    feat: add `StyledThemeProvider`to `_app`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { StyledThemeProvider } from 'styles/theme'
+import GlobalStyle from 'styles/globalStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyledThemeProvider>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </StyledThemeProvider>
+  )
 }
 
 export default MyApp
