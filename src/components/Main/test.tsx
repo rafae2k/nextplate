0133b1ba7bb122a677a3bb5985d55f335e8d3246
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-09 00:11:01 -0300

    test: add exemple test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import { Main } from '.'
+
+describe('<Main/>', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main>Hello World</Main>)
+
+    const heading = screen.getByRole('heading', { name: /Hello World/i })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
