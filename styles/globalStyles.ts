0afaf374d849dc734a9e3b26b9c0a1df2dfe2458
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-09 13:03:09 -0300

    style: add `globalStyles`

---
+++ styles/globalStyles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+ /* ====  RESET ==== */
+*,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+    font-family: 'Inter', sans-serif;
+    min-height: 100vh;
+    background-color: ${({ theme }) => theme.color.background};
+    color: ${({ theme }) => theme.color.heading};
+  }
+  ul {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+export default GlobalStyle
